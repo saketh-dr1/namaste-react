4e89b2f9880a90ef87f91ba91378a24d474c03eb
saketh-dr <donthireddy.s@mobiusdtaas.ai>
2025-08-13 12:48:34 +0530

    react from scratch

---
+++ app.js
@@ -0,0 +1,11 @@
+const heading = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag")
+ ]),React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag")
+ ])]);
+
+const root = ReactDOM.createRoot(document.getElementById("root")); // everything is rendered in the root
+
+root.render(heading);
